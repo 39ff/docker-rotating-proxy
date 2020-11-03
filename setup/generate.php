80ee155c9df100f1d432eab456df355273a72252
kouhei <admin@cse.ne.jp>
2020-11-03 20:44:44 +0900

    updated

--- setup/generate.php
+++ setup/generate.php
@@ -6,6 +6,7 @@ use Symfony\Component\Yaml\Yaml;
 copy('../template/squid.conf','./squid.conf');
 $to = Yaml::parseFile('../template/docker-compose.yml');
 $proxies = fopen('../proxyList.txt','r');
+$firewall = trim(file_get_contents('../firewall.txt'));
 
 $i = 1;
 $port = 49152;
@@ -47,9 +48,11 @@ while ($line = fgets($proxies)){
         ],
         'image' => 'chenhw2/gost:latest',
         'environment' => [
-            'ARGS' => sprintf('-L=:%d -F=%s://%s%s:%d', $port, $proxyInfo[2], $cred, $proxyInfo[0], $proxyInfo[1])
+            'ARGS' => sprintf('-L=%s:%d -F=%s://%s%s:%d',$firewall, $port, $proxyInfo[2], $cred, $proxyInfo[0], $proxyInfo[1])
         ]
     ];
+    file_put_contents('./squid.conf',PHP_EOL.sprintf('cache_peer %s parent %d 0 no-digest no-netdb-exchange connect-fail-limit=10 connect-timeout=8 round-robin no-query allow-miss proxy-only name=gost%d','127.0.0.1',$port,$i),FILE_APPEND);
+
     $i++;
     $port++;
 }
